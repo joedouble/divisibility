ed0d9a05078d936ecc3a87cfaeeee22429c53396
Joseph Double <josephdouble@Josephs-MacBook-Pro.local>
2019-01-17 16:29:06 -0500

    Adding code that currently doesn't work but is pointing me in the right direction.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,22 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        int number = 10;
+        int smallestNumber = 0;
+        boolean isItAMultiple = true;
+
+        for (int counter = 2; counter <= number; counter++) {
+
+            for (int multiple = 1; multiple <= number; multiple++) {
+                if (counter % multiple != 0) {
+                    isItAMultiple = false;
+                }
+            }
+            if (isItAMultiple = true) {
+                smallestNumber = counter;
+            }
+        }
+        System.out.println("The smallest number is " + smallestNumber);
     }
 }
