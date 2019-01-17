2f8b240e9252fbe391398dad0fda40cd40dc4e7d
Joseph Double <josephdouble@Josephs-MacBook-Pro.local>
2019-01-17 17:02:37 -0500

    Adding notes so that I'll remember what my train of thought was.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,8 +5,17 @@ public class Main {
     public static void main(String[] args) {
 
         int smallestNumber = 1;
-        int maxDivisor = 10;
+        int maxDivisor = 20;
 
+
+        /*  Joe Notes -
+        This for loop will compare a counter against what is currently assigned to the
+        smallestNumber variable.  If the remainder of the comparison is equal to 0, then
+        the counter increases and then the smallest number is compared against the updated counter.
+
+        If the comparison doesn't equal 0, then reset the counter (because the counter will need to be used
+        again for the updated smallestNumber), and increment up the smallestNumber.
+         */
         for (int counter = 1; counter <= maxDivisor; counter++) {
             if (smallestNumber % counter != 0) {
                 counter = 1;
